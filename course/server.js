9e228c5140c4656a11833db60dae764631451195
yulianim <yuliani_m@hotmail.com>
2022-07-26 21:28:17 -0500

    Adding basic routing

--- course/server.js
+++ course/server.js
@@ -4,7 +4,21 @@ const server=http.createServer((req, res)=>{
     console.log(req.url, req.method);
     //Set header content type
     res.setHeader('Content-Type', 'text/html');
-    fs.readFile('./views/index.html', (err, data)=>{
+
+    let path='./views/';
+    switch(req.url){
+        case '/':
+            path +='index.html';
+            break;
+        case '/about':
+            path +='about.html';
+            break;
+        default: 
+            path += '404.html';
+            break;
+    }
+
+    fs.readFile(path, (err, data)=>{
         if(err){
             console.log(err);
             res.end();
