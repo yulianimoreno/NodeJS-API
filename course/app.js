779b8decad3b7a8c1ff5c5c385a755a11ea5481b
yulianim <yuliani_m@hotmail.com>
2022-07-27 14:01:56 -0500

    Passing data into the views

--- course/app.js
+++ course/app.js
@@ -6,19 +6,24 @@ app.set('views', 'views-ejs');
 
 app.listen(3000);
 app.get('/', (req, res)=>{
-    //res.send('<p>Home page</p>');
-    res.render('index')
+    const blogs=[
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consecutor'},
+        {title: 'Mario finds eggs', snippet: 'Lorem ipsum dolor sit amet consecutor'},
+        {title: 'How to build an app', snippet: 'Lorem ipsum dolor sit amet consecutor'},
+        {title: 'How to connect the database with java', snippet: 'Lorem ipsum dolor sit amet consecutor'},
+    ];
+    res.render('index', {title: 'Home', blogs:blogs});
 });
 app.get('/about', (req, res)=>{
     //res.send('<p>About page</p>');
-    res.render('about');
+    res.render('about', {title: 'About'});
 });
 
 app.get('/blogs/create', (req, res)=>{
-    res.render('create')
+    res.render('create', {title: 'Create a new blog'})
 });
 
 //404 page
 app.use((req, res)=>{
-    res.status(404).render('404');
+    res.status(404).render('404', {title: 'Not found'});
 });
