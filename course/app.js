9f090d76d33d9b29f87e523469c7477cb2d987da
yulianim <yuliani_m@hotmail.com>
2022-07-27 10:56:11 -0500

    Adding ejs view engine

--- course/app.js
+++ course/app.js
@@ -1,22 +1,24 @@
 const express=require('express');
 const app=express();
+//Register view engine 
+app.set('view engine', 'ejs');
+app.set('views', 'views-ejs');
 
 app.listen(3000);
 app.get('/', (req, res)=>{
     //res.send('<p>Home page</p>');
-    res.sendFile('./views/index.html', {root: __dirname});
+    res.render('index')
 });
 app.get('/about', (req, res)=>{
     //res.send('<p>About page</p>');
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about');
 });
 
-//Redirects
-app.get('/about-us', (req, res)=>{
-    res.redirect('/about');
+app.get('/blogs/create', (req, res)=>{
+    res.render('create')
 });
 
 //404 page
 app.use((req, res)=>{
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404');
 });
