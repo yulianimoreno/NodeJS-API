9c3f4fe7cd591806f04e77e8917cab48950f793d
yulianim <yuliani_m@hotmail.com>
2022-07-26 21:10:41 -0500

    Adding response headers

--- course/server.js
+++ course/server.js
@@ -1,7 +1,13 @@
 const http =require('http');
 const server=http.createServer((req, res)=>{
-    console.log('request made ');
+    console.log(req.url, req.method);
+    //Set header content type
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<p>Hello, world</p>');
+    res.write('<p>Hello again, world</p>');
+    res.end();
 });
+
 server.listen(3000, 'localhost', ()=>{
     console.log('Listening for request on por 30000');
-})
+});
