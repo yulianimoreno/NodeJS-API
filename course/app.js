7e3a61fd66e3395f04e5e3f6de1bf67735a90d2a
yulianim <yuliani_m@hotmail.com>
2022-07-26 22:20:33 -0500

    Routing to html page using express

--- course/app.js
+++ course/app.js
@@ -3,5 +3,10 @@ const app=express();
 
 app.listen(3000);
 app.get('/', (req, res)=>{
-    res.send('<p>Home page</p>');
+    //res.send('<p>Home page</p>');
+    res.sendFile('./views/index.html', {root: __dirname});
+});
+app.get('/about', (req, res)=>{
+    //res.send('<p>About page</p>');
+    res.sendFile('./views/about.html', {root: __dirname});
 });
