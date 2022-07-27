0615a48c530e140a2eefe2246a67533ac8aefaaa
yulianim <yuliani_m@hotmail.com>
2022-07-26 21:18:44 -0500

    Returnig a html page in the response

--- course/server.js
+++ course/server.js
@@ -1,11 +1,19 @@
 const http =require('http');
+const fs=require('fs');
 const server=http.createServer((req, res)=>{
     console.log(req.url, req.method);
     //Set header content type
     res.setHeader('Content-Type', 'text/html');
-    res.write('<p>Hello, world</p>');
-    res.write('<p>Hello again, world</p>');
-    res.end();
+    fs.readFile('./views/index.html', (err, data)=>{
+        if(err){
+            console.log(err);
+            res.end();
+        }else{
+            res.write(data)
+            res.end();
+        }
+        
+    });
 });
 
 server.listen(3000, 'localhost', ()=>{
