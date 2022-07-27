0bc90ad9d412a7fb7e2f4b590f8241e2873fbce5
yulianim <yuliani_m@hotmail.com>
2022-07-26 20:18:57 -0500

    Adding a basic server

---
+++ course/server.js
@@ -0,0 +1,7 @@
+const http =require('http');
+const server=http.createServer((req, res)=>{
+    console.log('request made ');
+});
+server.listen(3000, 'localhost', ()=>{
+    console.log('Listening for request on por 30000');
+})
